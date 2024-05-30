3f33252b4c627267896aace84e1a9d28349435c7
sturmenta <sturmenta@gmail.com>
2024-05-30 00:18:35 -0300

    add some animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { motion } from "framer-motion"
 import { useState } from "react"
 
 import { BgAnimatedGradient } from "@/components/generic/bg-animated-gradient"
@@ -7,25 +8,49 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
 export default function Home() {
-  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
 
   return (
     <BgAnimatedGradient>
       <main className="flex min-h-screen flex-col items-center justify-center text-white">
-        <p className="p-10 text-2xl">Sprint smart contract</p>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
+          className="flex flex-1 flex-col items-center justify-center">
+          <div className="flex">
+            <p className="px-5 text-center text-5xl">Sprint smart contracts</p>
+          </div>
+          <motion.div
+            className="mt-3 text-xl text-gray-600"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 1.5 }}>
+            <p className="px-5 text-center">
+              Your sprints with blockchain technology
+            </p>
+          </motion.div>
 
-        <Input
-          placeholder="Enter your name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="max-w-sm"
-        />
-        <div className="h-4" />
-        <Button
-          className="w-full max-w-sm"
-          onClick={() => alert(`Hello ${name}`)}>
-          Say Hello
-        </Button>
+          <div className="h-5" />
+
+          <motion.div
+            className="w-full max-w-sm p-5"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 3 }}>
+            <Input
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <div className="h-4" />
+            <Button
+              className="w-full"
+              onClick={() => alert(`Hello ${email}!!`)}>
+              Start!
+            </Button>
+          </motion.div>
+        </motion.div>
       </main>
     </BgAnimatedGradient>
   )
